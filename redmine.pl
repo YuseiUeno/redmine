545497f3291cfe62cfe0089d936286c2be4f60d0
Yusei Ueno <yusei@fout.jp>
2018-09-07 12:49:20 +0900

    user_url 廃止

--- redmine.pl
+++ redmine.pl
@@ -21,19 +21,10 @@ sub api_key {
     return $keys[0] || '';
 }
 
-sub user_url {
-    open (FILE, 'user_url.txt') or die "$!";
-    my @urls = <FILE>;
-    chomp(@urls);
-    close(FILE);
-
-    return $urls[0] || '';
-}
-
 sub get_issues {
     my $today = localtime(Time::Piece->strptime(localtime->ymd, "%Y-%m-%d"));
 
-    my $uri = URI->new(user_url);
+    my $uri = URI->new('https://redmine.fout.jp/projects/fout-dev/issues.json?utf8=✓&set_filter=1&sort=assigned_to%2Cpriority%3Adesc%2Cdue_date&f%5B%5D=category_id&op%5Bcategory_id%5D=%3D&v%5Bcategory_id%5D%5B%5D=11&v%5Bcategory_id%5D%5B%5D=17&v%5Bcategory_id%5D%5B%5D=12&f%5B%5D=tracker_id&op%5Btracker_id%5D=%3D&v%5Btracker_id%5D%5B%5D=6&v%5Btracker_id%5D%5B%5D=5&v%5Btracker_id%5D%5B%5D=2&v%5Btracker_id%5D%5B%5D=7&v%5Btracker_id%5D%5B%5D=3&v%5Btracker_id%5D%5B%5D=1&v%5Btracker_id%5D%5B%5D=8&v%5Btracker_id%5D%5B%5D=9&v%5Btracker_id%5D%5B%5D=10&v%5Btracker_id%5D%5B%5D=14&v%5Btracker_id%5D%5B%5D=13&v%5Btracker_id%5D%5B%5D=15&v%5Btracker_id%5D%5B%5D=28&f%5B%5D=start_date&op%5Bstart_date%5D=%3C%3D&v%5Bstart_date%5D%5B%5D=2018-09-03&f%5B%5D=due_date&op%5Bdue_date%5D=%3E%3D&v%5Bdue_date%5D%5B%5D=2018-08-20&f%5B%5D=&c%5B%5D=priority&c%5B%5D=tracker&c%5B%5D=due_date&c%5B%5D=subject&c%5B%5D=done_ratio&c%5B%5D=assigned_to&group_by=status&t%5B%5D=');
     $uri->query_param("key", api_key);
     $uri->query_param("v[start_date][]", $today->strftime("%Y-%m-%d"));
     $uri->query_param("v[due_date][]", ($today - ONE_DAY * $today->_wday)->strftime("%Y-%m-%d"));
